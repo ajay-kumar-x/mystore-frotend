c4d2620816d9dae873e46e04aa575221d3a39113
Ajay Kumar <79449188+ajaykr1729@users.noreply.github.com>
2023-03-08 00:29:05 +0530

    Delete Products directory

--- Products/src/main/java/org/mystore/exception/ImageNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.mystore.exception;
-
-public class ImageNotFoundException extends Exception {
-    public ImageNotFoundException(String message) {
-        super(message);
-    }
-}
--- Products/src/main/java/org/mystore/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.mystore.repository;
-
-import org.mystore.model.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
